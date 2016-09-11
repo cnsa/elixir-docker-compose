bd536425a353c9c6f0b6e97c3f14320031872c81
Alexander Merkulov <sasha@merqlove.ru>
2016-09-11 22:59:42 +0300

    Cleanup

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ set -e
 APP_NAME=some_app
 export VERSION=`mix $APP_NAME.version 2>&1 | sed -e '$!d'`
 
-DATA_C="build_data_$APP_NAME_$VERSION"
+DATA_C="build_data_""$APP_NAME""_$VERSION"
 B_I="project-build-some-app:$VERSION"
 
 docker rm -f $DATA_C 2>/dev/null || true
@@ -17,7 +17,7 @@ docker create -v /build/deps \
 docker build -f Dockerfile.build -t $B_I .
 docker run --volumes-from $DATA_C \
            -e MIX_ENV=prod --rm -t $B_I \
-           sh -c "mv config.exs rel/config.exs && mix deps.get && mix compile && mix release"
+           sh -c "mv config.exs rel/config.exs && mix do deps.get, compile, release"
 mkdir -p rel/$APP_NAME/releases/$VERSION
 docker cp $DATA_C:/build/rel/$APP_NAME/releases/$VERSION/$APP_NAME.tar.gz rel/$APP_NAME/releases/$VERSION/$APP_NAME.tar.gz
 docker rm -f $DATA_C 2>/dev/null || true
